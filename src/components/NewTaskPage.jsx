7a660529c245bdd05b6a4f2fd7a9e3066551bf85
swoody2527 <104504405+swoody2527@users.noreply.github.com>
2025-08-22 15:35:23 +0100

    can add a new task

--- src/components/NewTaskPage.jsx
+++ src/components/NewTaskPage.jsx
@@ -1,36 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router";
 import '../App.css'
+import axios from 'axios';
 
 function NewTaskPage() {
+
+    const [title, setTitle] = useState('')
+    const [desc, setDesc] = useState('')
+    const [status, setStatus] = useState('')
+    const [date, setDate] = useState('')
+
+    const [success, setSuccess] = useState(null)
+
+
+
+    const handleNewTaskSubmit = (e) => {
+        e.preventDefault()
+        try {
+            const res = axios.post('http://127.0.0.1:8000/task', {title: title, desc: desc, status: status, due: date})
+            setSuccess(true)
+            setTitle('')
+            setDesc('')
+            setStatus('')
+            setDate('')
+        } catch (err) {
+            console.log(err)
+        }
+
+    }
+
   return (
     <>
     <Link to="/"><button>Back</button></Link>
     <div className="form-container">
       <h1>New Task</h1>
-      <form>
+      {success ? <h2>Task Submitted!</h2> : null}
+      <form onSubmit={handleNewTaskSubmit}>
         <div className="task-option">
           <label htmlFor="title">Title:</label>
-          <input id="title" name="title" type="text" />
+          <input onChange={(e) => setTitle(e.target.value)} id="title" name="title" type="text" value={title} />
         </div>
 
         <div className="task-option">
           <label htmlFor="description">Description (Optional):</label>
-          <input id="description" name="description" type="text" />
+          <input onChange={(e) => setDesc(e.target.value)} id="description" name="description" type="text" value={desc} />
         </div>
 
         <div className="task-option">
           <label htmlFor="status">Status:</label>
-          <select>
-            <option>To Do</option>
-            <option>In Progress</option>
-            <option>Completed</option>
+          <select onChange={(e) => setStatus(e.target.value) } value={status}>
+            <option value='todo'>To Do</option>
+            <option value='in-progress'>In Progress</option>
+            <option value='completed'>Completed</option>
           </select>
         </div>
 
         <div className="task-option">
           <label htmlFor="due">Due:</label>
-          <input id="due" name="due" type="date" />
+          <input onChange={(e) => setDate(e.target.value)} id="due" name="due" type="date" value={date} />
         </div>
 
         <button type="submit" className="submit-btn">Save Task</button>
