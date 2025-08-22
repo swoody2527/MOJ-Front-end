1b1360e68275d897766e553a8cc06b3fa0715d7d
swoody2527 <104504405+swoody2527@users.noreply.github.com>
2025-08-22 11:46:00 +0100

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Route, Routes } from 'react-router'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  return (
+    <>
+    <div className='header'>MOJ Task Manager</div>
+    <div>
+
+    </div>
+    </>
+  )
+}
+
+export default App
